f43276c96bd461abcc5efb943355234d5c33cb4d
quake <31706007+justlikebtw@users.noreply.github.com>
2021-06-27 08:34:22 +0000

    h

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('GIF engellemeye hazır.'));
+app.get('/', (req, res) => res.send('.'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 const discord = require('discord.js');
 const client = new discord.Client()
-client.login('ODUyODkzMTkxODgyNTM5MDI5.YNgv_w.LRb3TJ6YLfEtR4cTAJaG2YeRKd0');
+client.login(process.env.TOKEN);
